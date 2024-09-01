feeed16568090998d3e5ccf44556b6ec81990a9f
AKIOMASAMORI <parkourbaseball0705@gmail.com>
2024-09-01 22:07:28 +0900

    otukunacha-ji

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,15 @@ export default function Home() {
       <h1 className="text-center text-4xl font-extrabold mb-6 tracking-widest uppercase relative z-10">
         TIKTOK
       </h1>
+      {/* お得なチャージリンク */}
+      <div className="flex justify-center mb-6">
+        <a
+          href="https://www.tiktok.com/coin?lang=ja-JP"
+          className="px-6 py-3 bg-blue-600 text-white font-medium text-lg rounded-lg shadow-md hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 transition duration-300 ease-in-out"
+        >
+          お得なチャージ
+        </a>
+      </div>
 
       <div className="bg-gradient-to-br from-gray-700 via-gray-800 to-black p-6 rounded-lg shadow-lg relative z-10 border border-gray-700">
         <h2 className="text-2xl font-semibold mb-4">Battle Results</h2>
